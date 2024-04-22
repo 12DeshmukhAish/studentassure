fc59c6a641ba8e809814ebe171dfc34faa7b6883
prathameshmane019 <maneprathamesh019@gmail.com>
2024-04-22 11:26:33 +0530

    evalution added

--- app/(main)/super_admin/evaluate/page.js
+++ app/(main)/super_admin/evaluate/page.js
@@ -1,226 +1,160 @@
 "use client"
-import React, { useState,useEffect } from 'react';
-import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Button } from '@/components/ui/button';
 const EvaluationPage = () => {
+  const [feedbackData, setFeedbackData] = useState([]);
+  const [selectedFeedbackId, setSelectedFeedbackId] = useState('');
+  const [selectedSubject, setSelectedSubject] = useState('');
   const [selectedFeedback, setSelectedFeedback] = useState(null);
-  const [selectedFaculty, setSelectedFaculty] = useState('');
-  const [feedbackData, setFeedbackData] = useState([
-    
-  {
-      "_id": "66250af5e4e04c09b251e616",
-      "feedback_id": "662504bce4e04c09b251e60c",
-      "ratings": [
-          {
-              "subject_id": "s111",
-              "suggestions": "No suggetion",
-              "ratings": [
-                  5,
-                  4,
-                  5,
-                  4,
-                  5,
-                  4,
-                  5
-              ],
-              "_id": "66250af5e4e04c09b251e617"
-          },
-          {
-              "subject_id": "s112",
-              "suggestions": "",
-              "ratings": [
-                  5,
-                  5,
-                  5,
-                  5,
-                  4,
-                  5,
-                  4
-              ],
-              "_id": "66250af5e4e04c09b251e618"
-          },
-          {
-              "subject_id": "s113",
-              "suggestions": "",
-              "ratings": [
-                  5,
-                  4,
-                  5,
-                  5,
-                  5,
-                  5,
-                  5
-              ],
-              "_id": "66250af5e4e04c09b251e619"
-          },
-          {
-              "subject_id": "s114",
-              "suggestions": "",
-              "ratings": [
-                  5,
-                  5,
-                  5,
-                  1,
-                  1,
-                  5,
-                  5
-              ],
-              "_id": "66250af5e4e04c09b251e61a"
-          },
-          {
-              "subject_id": "s115",
-              "suggestions": "",
-              "ratings": [
-                  5,
-                  5,
-                  5,
-                  5,
-                  1,
-                  5,
-                  5
-              ],
-              "_id": "66250af5e4e04c09b251e61b"
-          },
-          {
-              "subject_id": "s116",
-              "suggestions": "",
-              "ratings": [
-                  5,
-                  5,
-                  5,
-                  5,
-                  5,
-                  5,
-                  5
-              ],
-              "_id": "66250af5e4e04c09b251e61c"
-          },
-          {
-              "subject_id": "s117",
-              "suggestions": "",
-              "ratings": [
-                  5,
-                  5,
-                  5,
-                  5,
-                  5,
-                  5,
-                  5
-              ],
-              "_id": "66250af5e4e04c09b251e61d"
-          }
-      ],
-      "date": "2024-04-21T12:47:49.656Z",
-      "createdAt": "2024-04-21T12:47:49.673Z",
-      "updatedAt": "2024-04-21T12:47:49.673Z",
-      "__v": 0
-  }
-  ]);
-
-  const handleFeedbackChange = (event) => {
-    const feedbackId = event.target.value;
-    const feedback = feedbackData.find((item) => item.feedback_id === feedbackId);
-    setSelectedFeedback(feedback);
-  };
-
-  const handleFacultyChange = (event) => {
-    setSelectedFaculty(event.target.value);
-  };
-
-  const printPage = () => {
+  const [response, setResponses] = useState([]);
+  const printDiv = () => {
+    const printContents = document.getElementById('table-to-print').innerHTML;
+    const originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
     window.print();
+    document.body.innerHTML = originalContents;
+  };
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = date.getFullYear();
+    return dd +"/"+ mm+"/" + yyyy;
   };
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchFeedbackData = async () => {
       try {
         const response = await axios.get('/api/feedbackData');
         setFeedbackData(response.data.feedbackData);
       } catch (error) {
-        setError('Error fetching feedback data');
+        console.error('Error fetching feedback data:', error);
       }
-      
     };
 
-    fetchData();
+    fetchFeedbackData();
   }, []);
 
-  const storePage = () => {
-    const pageContent = document.documentElement.outerHTML;
-    const blob = new Blob([pageContent], { type: 'text/html' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = 'evaluation_page.html';
-    link.click();
+  useEffect(() => {
+    if (selectedFeedback) {
+      setSelectedFeedbackId(selectedFeedback._id)
+      const fetchFeedbackResponses = async () => {
+        try {
+          const response = await axios.get(`/api/findResponses?feedbackId=${selectedFeedbackId}`);
+          setResponses(response.data);
+        } catch (error) {
+          console.error('Error fetching responses:', error);
+          setResponses([]);
+        }
+      };
+
+      fetchFeedbackResponses();
+    }
+
+  }, [selectedFeedback, selectedSubject, selectedFeedbackId]);
+
+  const calculateEvaluationPoint = (questionIndex) => {
+    let totalPoints = 0;
+    let totalRatings = 0;
+
+    response.forEach((feedbackEntry) => {
+      const ratingsForSubject = feedbackEntry.ratings.find(rating => rating.subject_id === selectedSubject._id);
+      if (ratingsForSubject) {
+        const rating = ratingsForSubject.ratings[questionIndex];
+        if (!isNaN(rating) && rating !== null) {
+          totalPoints += rating;
+          totalRatings++;
+        }
+      }
+    });
+
+    return totalRatings > 0 ? totalPoints / totalRatings : 0;
   };
 
-  const questions = [
-    "How clearly the teacher delivered the lecture topics? (Clarity and understandability of teacher's explanation and his full preparation of topic)",
-    "Are the lectures delivered by the teacher interactive and interesting?",
-    "How do you rate the communication of the teachers? Such as skills include use of chalk, duster, blackboard, LCD, OHP, way of speaking",
-    "How do you rate the teacher to answer your questions & solve your difficulties?",
-    "How do you rate the speed at which the teacher speaks while delivering the lecture?",
-    "How do you rate about teacher regularity and punctuality? (Engage 100% lectures as per the timetable, complete topic in time, punctual to the class)",
-    "How do you rate the attitude & behavior of the teacher towards students within & outside the class?"
-  ];
+  const calculateRatingCounts = (questionIndex) => {
+    const ratingCounts = { Poor: 0, Average: 0, Good: 0, VeryGood: 0, Excellent: 0 };
 
-  const calculateEvaluationPoint = (feedback) => {
-    const ratings = feedback.ratings.flatMap((rating) => rating.ratings);
-    const ratingCounts = ratings.reduce((counts, rating) => {
-      counts[rating] = (counts[rating] || 0) + 1;
-      return counts;
-    }, {});
+    response.forEach((feedbackEntry) => {
+      const ratingsForSubject = feedbackEntry.ratings.find(rating => rating.subject_id === selectedSubject._id);
+      if (ratingsForSubject) {
+        const rating = ratingsForSubject.ratings[questionIndex];
+        if (!isNaN(rating) && rating !== null) {
+          if (rating === 1) ratingCounts.Poor++;
+          else if (rating === 2) ratingCounts.Average++;
+          else if (rating === 3) ratingCounts.Good++;
+          else if (rating === 4) ratingCounts.VeryGood++;
+          else if (rating === 5) ratingCounts.Excellent++;
+        }
+      }
+    });
 
-    const poor = ratingCounts[1] || 0;
-    const average = ratingCounts[2] || 0;
-    const good = ratingCounts[3] || 0;
-    const veryGood = ratingCounts[4] || 0;
-    const excellent = ratingCounts[5] || 0;
-    return poor * 1 + average * 2 + good * 3 + veryGood * 4 + excellent * 5;
+    return ratingCounts;
   };
 
+  const calculateTotalPoints = () => {
+    let total = 0;
+
+    response.forEach((feedbackEntry) => {
+      const ratingsForSubject = feedbackEntry.ratings.find(rating => rating.subject_id === selectedSubject._id);
+      if (ratingsForSubject) {
+        ratingsForSubject.ratings.forEach((rating) => {
+          if (!isNaN(rating) && rating !== null) {
+            total += rating;
+          }
+        });
+      }
+    });
+
+    return total;
+  };
+
+  const calculateAveragePoints = () => {
+    const totalPoints = calculateTotalPoints();
+    const totalQuestions = selectedFeedback.questions.length;
+    return totalPoints / (response.length * totalQuestions);
+  };
+   
   return (
     <div>
-      <Head>
-        <title>Faculty Evaluation</title>
-      </Head>
+      <h1>Faculty Evaluation</h1>
       <div className="container mx-auto px-4 py-8">
         <div className="mb-4">
-          <label htmlFor="feedbackSelect" className="block font-bold mb-2">
-            Select Feedback:
-          </label>
-          <select
-            id="feedbackSelect"
-            value={selectedFeedback?.feedback_id || ''}
-            onChange={handleFeedbackChange}
-            className="border border-gray-300 rounded-md px-4 py-2"
-          >
-            <option value="">Select Feedback</option>
-            {feedbackData.map((feedback) => (
-              <option key={feedback.feedback_id} value={feedback.feedback_id}>
-                {feedback.feedback_id}
-              </option>
-            ))}
-          </select>
+          <Select defaultValue={selectedFeedbackId} onValueChange={(value) => setSelectedFeedback(value)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select a Feedback" />
+            </SelectTrigger>
+            <SelectContent>
+              {feedbackData.map((feedback) => (
+                <SelectItem key={feedback._id} value={feedback}>
+                  {feedback.feedbackTitle}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
+
         <div className="mb-4">
-          <label htmlFor="facultyInput" className="block font-bold mb-2">
-            Enter Faculty Name:
-          </label>
-          <input
-            id="facultyInput"
-            type="text"
-            value={selectedFaculty}
-            onChange={handleFacultyChange}
-            className="border border-gray-300 rounded-md px-4 py-2"
-          />
+          <Select defaultValue={selectedSubject} onValueChange={(value) => setSelectedSubject(value)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select a Subject" />
+            </SelectTrigger>
+            <SelectContent>
+              {selectedFeedback && selectedFeedback.subjects.map((subject) => (
+                <SelectItem key={subject._id} value={subject}>
+                  {subject.faculty}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
-        {selectedFeedback && (
-          <div className="bg-white rounded-lg shadow-lg p-6">
+
+        {response.length > 0 && (
+          <div id="table-to-print" className="bg-white rounded-lg shadow-lg p-6">
             <h2 className="text-2xl font-bold mb-4">
-              Feedback Id: {selectedFeedback.feedback_id} Faculty Name: {selectedFaculty} Subject: AM-2
+              Feedback Title: {selectedFeedback?.feedbackTitle} Faculty Name: {selectedSubject.faculty} Subject: {selectedSubject.subject}
             </h2>
-            <p className="mb-4">Date of Feedback: 03/01/2024 Total Feedbacks: 69</p>
+            <p className="mb-4">Date of Feedback: {formatDate(response[0].date)} Total Feedbacks: {response.length}</p>
             <table className="w-full table-auto">
               <thead>
                 <tr>
@@ -235,58 +169,31 @@ const EvaluationPage = () => {
                 </tr>
               </thead>
               <tbody>
-                {questions.map((question, index) => {
-                  const evaluationPoint = calculateEvaluationPoint(selectedFeedback);
-                  const ratings = selectedFeedback.ratings.flatMap((rating) => rating.ratings);
-                  const ratingCounts = ratings.reduce((counts, rating) => {
-                    counts[rating] = (counts[rating] || 0) + 1;
-                    return counts;
-                  }, {});
-
-                  const poor = ratingCounts[1] || 0;
-                  const average = ratingCounts[2] || 0;
-                  const good = ratingCounts[3] || 0;
-                  const veryGood = ratingCounts[4] || 0;
-                  const excellent = ratingCounts[5] || 0;
-
-                  return (
-                    <tr key={index}>
-                      <td className="border px-4 py-2">{index + 1}</td>
-                      <td className="border px-4 py-2">{question}</td>
-                      <td className="border px-4 py-2">{poor}</td>
-                      <td className="border px-4 py-2">{average}</td>
-                      <td className="border px-4 py-2">{good}</td>
-                      <td className="border px-4 py-2">{veryGood}</td>
-                      <td className="border px-4 py-2">{excellent}</td>
-                      <td className="border px-4 py-2">{evaluationPoint}</td>
-                    </tr>
-                  );
-                })}
+                {selectedFeedback && selectedFeedback?.questions.map((question, index) => (
+                  <tr key={index}>
+                    <td className="border px-4 py-2">{index + 1}</td>
+                    <td className="border px-4 py-2">{question}</td>
+                    {Object.values(calculateRatingCounts(index)).map((count, i) => (
+                      <td key={i} className="border px-4 py-2">{count}</td>
+                    ))}
+                    <td className="border px-4 py-2">{calculateEvaluationPoint(index).toFixed(2)}</td>
+                  </tr>
+                ))}
               </tbody>
               <tfoot>
                 <tr>
                   <td colSpan="7" className="text-right font-bold pr-4">Total</td>
-                  <td className="border px-4 py-2">
-                    {calculateEvaluationPoint(selectedFeedback)}
-                  </td>
+                  <td className="border px-4 py-2">{calculateTotalPoints()}</td>
                 </tr>
                 <tr>
                   <td colSpan="7" className="text-right font-bold pr-4">Average</td>
-                  <td className="border px-4 py-2">
-                    {(
-                      calculateEvaluationPoint(selectedFeedback) / selectedFeedback.ratings.length
-                    ).toFixed(2)}
-                  </td>
+                  <td className="border px-4 py-2">{calculateAveragePoints().toFixed(2)}</td>
                 </tr>
               </tfoot>
             </table>
-            <div className="mt-6">
-              <h3 className="text-xl font-bold mb-2">Suggestions</h3>
-              <p>Sir you will give the best experience of teaching</p>
-              <p>Good understanding</p>
-            </div>
           </div>
         )}
+         <Button onClick={printDiv}>Print</Button>
       </div>
     </div>
   );
